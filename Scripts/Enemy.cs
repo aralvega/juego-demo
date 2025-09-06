300c91207976ff9a04ae69ddac6162e89a32dee0
ArielAVega <avega@fi.unju.edu.ar>
2025-09-06 00:21:06 -0300

    Add Enemy script with health and damage (#2)

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy defeated!");
+        Destroy(gameObject);
+    }
+}
